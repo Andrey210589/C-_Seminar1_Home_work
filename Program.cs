9d39d9e9e9bfdfd8aba5eb40437860c75ef5607c
Andrey210589 <PolishhukAk@mail.ru>
2023-03-24 21:52:32 +0300

    Task 1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input a ferst number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 <= num2)
+{
+    
+    Console.WriteLine("max num2:  " + num2);
+}
+else 
+ {
+    Console.WriteLine("max num1:  " + num1);
+ }
