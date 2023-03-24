5d8e6acb0fd520c5f0e208d83c5a2ca32e98fdd3
Andrey210589 <PolishhukAk@mail.ru>
2023-03-24 21:55:12 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Input a ferst number ");
+﻿// Task 2 Напишите программу которая на вход принимает два числа и 
+// выдает какое из них меньше.
+
+Console.WriteLine("Input a ferst number ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input a second number");
 int num2 = Convert.ToInt32(Console.ReadLine());
@@ -6,9 +9,9 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 <= num2)
 {
     
-    Console.WriteLine("max num2:  " + num2);
+    Console.WriteLine("min num1:  " + num1);
 }
 else 
  {
-    Console.WriteLine("max num1:  " + num1);
+    Console.WriteLine("max num2:  " + num2);
  }
